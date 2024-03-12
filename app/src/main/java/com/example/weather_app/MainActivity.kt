98425c6300d260a5dd339079d2535916b5db83c9
avazbek <avazbekdeveloper11@gmail.com>
2024-03-12 17:06:05 +0500

    first commit

---
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.weather_app
+
+import MainViewModel
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.ViewModelProvider
+import com.example.weather_app.ui.theme.Weather_appTheme
+
+class MainActivity : ComponentActivity() {
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Weather_appTheme {
+                Column(Modifier.fillMaxSize()) {
+                    Image(
+                        painter = painterResource(id = R.drawable.forest_sunny),
+                        contentDescription = "Background",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color(0xFF47AB2F ))
+                    )
+                    Box(
+                        modifier = Modifier
+                            .background(Color(0xFF47AB2F))
+                            .fillMaxSize()
+                    ) {
+                        Text("TODO")
+                    }
+                }
+            }
+
+        }
+        requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+}
+ 
---
+++ app/src/main/java/com/example/weather_app/service/openWeatherService.kt
@@ -0,0 +1,20 @@
+package com.example.weather_app.service
+
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenWeatherService {
+    @GET("weather?lat={lat)&Lon={Lon}&appid=$(BuildConfig.API_KEY}&units=metric")
+    fun getCurrentWeather(
+        @Query("lat") lat: Double, @Query("long") long: Double
+    ): Response<ResponseBody>
+}
+
+fun OpenWeatherService(): OpenWeatherService =
+    Retrofit.Builder().baseUrl("https://api.gpenweathermap.org/data/2.5/")
+        .addConverterFactory(GsonConverterFactory.create()).build().create()
