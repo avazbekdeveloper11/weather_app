012a5fa8681069b99d9bb91d7010049f0ac0eaca
avazbek <avazbekdeveloper11@gmail.com>
2024-03-15 00:10:06 +0500

    api connected

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -21,8 +22,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.weather_app.ui.theme.Weather_appTheme
 
 class MainActivity : ComponentActivity() {
+    private  val viewModel : MainViewModel by viewModels()
     private val requestPermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) {}
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if(it) viewModel.onPermissionGranted()
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,11 +46,10 @@ class MainActivity : ComponentActivity() {
                             .background(Color(0xFF47AB2F))
                             .fillMaxSize()
                     ) {
-                        Text("TODO")
+                        Text("Mavlonov Avazbek")
                     }
                 }
             }
-
         }
         requestPermission.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         ViewModelProvider(this)[MainViewModel::class.java]
---
+++ app/src/main/java/com/example/weather_app/service/OpenWeatherService.kt
@@ -0,0 +1,27 @@
+package com.example.weather_app.service
+
+import com.example.weather_app.service.dto.WeatherResponse
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface OpenWeatherService {
+    @GET("weather?units=metric")
+    suspend fun getCurrentWeather(
+        @Query("lat") lat: Double,
+        @Query("lon") long: Double,
+        @Query("appid") apiKey: String
+    ): Response<WeatherResponse>
+}
+
+
+fun OpenWeatherService(): OpenWeatherService =
+    Retrofit.Builder().baseUrl("https://api.openweathermap.org/data/2.5/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create()
+
+
