6f413ec6fc7c3a90aa8e3b90f7f9b6a42926af42
avazbek <avazbekdevleoper11@gmail.com>
2024-04-06 01:57:34 +0500

    complete app

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.weather_app
 
 import MainViewModel
 import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -22,7 +27,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
@@ -40,6 +44,7 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import com.example.weather_app.service.dto.Forecast
 import com.example.weather_app.service.dto.Weather
 import com.example.weather_app.service.dto.WeatherResponse
@@ -48,8 +53,6 @@ import com.example.weather_app.ui.theme.RainyBlue
 import com.example.weather_app.ui.theme.SunGreen
 import com.example.weather_app.ui.theme.WeatherAppTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.PermissionRequired
-import com.google.accompanist.permissions.rememberPermissionState
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import java.sql.Date
 import java.text.SimpleDateFormat
@@ -59,39 +62,40 @@ import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
+    private val requestLocationPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+        if (isGranted) {
+            checkLocationServices()
+        }
+    }
+
 
     @ExperimentalPermissionsApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WeatherAppTheme {
-                val permission =
-                    rememberPermissionState(permission = Manifest.permission.ACCESS_FINE_LOCATION)
-                PermissionRequired(permissionState = permission, permissionNotGrantedContent = {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-
-                    ) {
-                        Text("Allow the location")
-                        Button(onClick = { permission.launchPermissionRequest() }) { Text("Give permission") }
-                    }
-                }, permissionNotAvailableContent = {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-
-                    ) {
-                        Text("Allow the location")
-                        Button(onClick = { permission.launchPermissionRequest() }) { Text("Give permission") }
-                    }
-                }) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    checkLocationServices()
                     MainScreen(viewModel = viewModel)
+
+                } else {
+                    requestLocationPermission()
                 }
             }
         }
+
+    }
+
+    private fun checkLocationServices() {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        }
+    }
+
+    private fun requestLocationPermission() {
+        requestLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
 
@@ -110,9 +114,7 @@ fun MainScreen(viewModel: MainViewModel) {
     val forecast by viewModel.forecast.collectAsState(emptyList())
     val backgroundColor = weather?.weather?.firstOrNull()?.main?.let { weatherBackgroundColor(it) }
 
-    rememberSystemUiController().setStatusBarColor(
-        backgroundColor ?: SunGreen
-    )
+    rememberSystemUiController().setStatusBarColor(backgroundColor ?: SunGreen)
 
     Column(Modifier.fillMaxSize()) {
         weather?.let {
@@ -120,15 +122,11 @@ fun MainScreen(viewModel: MainViewModel) {
             TemperatureSummary(weather = it, backgroundColor ?: Color.White)
             HorizontalDivider()
         }
-        Box(
-            modifier = Modifier
+        Box(modifier = Modifier
                 .background(backgroundColor ?: Color.White)
-                .fillMaxSize()
-        ) {
+                .fillMaxSize()) {
             if (weather == null || forecast.isEmpty()) {
-                CircularProgressIndicator(
-                    modifier = Modifier.align(Alignment.Center), color = Color(0xFF47AB2F)
-                )
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center), color = Color(0xFF47AB2F))
             } else {
                 FiveDayForecast(forecast = forecast, backgroundColor ?: Color.White)
             }
@@ -138,45 +136,23 @@ fun MainScreen(viewModel: MainViewModel) {
 
 @Composable
 fun FiveDayForecast(forecast: List<Forecast?>, backgroundColor: Color) {
-    LazyColumn(
-        modifier = Modifier.background(Color(0xFF536DFE)),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-    ) {
+    LazyColumn(modifier = Modifier.background(Color(0xFF536DFE)), contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
         items(forecast) { dayForecast ->
             Card(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp),
-                RoundedCornerShape(16.dp),
+                    Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                    RoundedCornerShape(16.dp),
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
+                Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier
                         .background(backgroundColor)
-                        .padding(horizontal = 16.dp, vertical = 12.dp)
-                ) {
-                    Text(
-                        text = SimpleDateFormat(
-                            "EEEE",
-                            Locale.getDefault()
-                        ).format(Date((dayForecast?.dt ?: 0) * 1_000)),
-                        color = Color.White,
-                        fontSize = 18.sp,
-                        modifier = Modifier.weight(1f)
-                    )
-                    Icon(
-                        painter = painterResource(
-                            dayForecast?.forecastIcon() ?: R.drawable.forest_sunny
-                        ),
-                        contentDescription = "Forecast icon",
-                        tint = Color.White,
-                        modifier = Modifier.size(32.dp)
-                    )
-                    Text(
-                        text = formatTemperature(dayForecast?.main?.temp ?: 0.0),
-                        color = Color.White,
-                        fontSize = 18.sp
-                    )
+                        .padding(horizontal = 16.dp, vertical = 12.dp)) {
+                    Text(text = SimpleDateFormat("EEEE", Locale.getDefault()).format(Date((dayForecast?.dt
+                            ?: 0) * 1_000)), color = Color.White, fontSize = 18.sp, modifier = Modifier.weight(1f))
+                    Icon(painter = painterResource(dayForecast?.forecastIcon()
+                            ?: R.drawable.forest_sunny), contentDescription = "Forecast icon", tint = Color.White, modifier = Modifier.size(32.dp))
+                    Text(text = formatTemperature(dayForecast?.main?.temp
+                            ?: 0.0), color = Color.White, fontSize = 18.sp)
                 }
             }
         }
@@ -186,46 +162,29 @@ fun FiveDayForecast(forecast: List<Forecast?>, backgroundColor: Color) {
 
 @Composable
 private fun TemperatureSummary(weather: WeatherResponse?, backgroundColor: Color) {
-    Box(
-        modifier = Modifier
+    Box(modifier = Modifier
             .fillMaxWidth()
             .background(backgroundColor)
             .padding(horizontal = 28.dp, vertical = 8.dp)
-            .background(
-                color = Color(0xFF536DFE), shape = RoundedCornerShape(16.dp)
-            )
-            .padding(vertical = 16.dp), contentAlignment = Alignment.Center
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            TemperatureItem(
-                label = "Min", temperature = formatTemperature(weather?.main?.tempMin ?: 0.0)
-            )
-            TemperatureItem(
-                label = "Now", temperature = formatTemperature(weather?.main?.temp ?: 0.0)
-            )
-            TemperatureItem(
-                label = "Max", temperature = formatTemperature(weather?.main?.tempMax ?: 0.0)
-            )
+            .background(color = Color(0xFF536DFE), shape = RoundedCornerShape(16.dp))
+            .padding(vertical = 16.dp), contentAlignment = Alignment.Center) {
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically) {
+            TemperatureItem(label = "Min", temperature = formatTemperature(weather?.main?.tempMin
+                    ?: 0.0))
+            TemperatureItem(label = "Now", temperature = formatTemperature(weather?.main?.temp
+                    ?: 0.0))
+            TemperatureItem(label = "Max", temperature = formatTemperature(weather?.main?.tempMax
+                    ?: 0.0))
         }
     }
 }
 
 @Composable
 private fun TemperatureItem(label: String, temperature: String) {
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = temperature, fontSize = 20.sp, color = Color.White
-        )
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = temperature, fontSize = 20.sp, color = Color.White)
         Spacer(modifier = Modifier.height(4.dp))
-        Text(
-            text = label, fontSize = 14.sp, color = Color.White.copy(alpha = 0.7f)
-        )
+        Text(text = label, fontSize = 14.sp, color = Color.White.copy(alpha = 0.7f))
     }
 }
 
@@ -238,44 +197,20 @@ private fun WeatherResponse.getWeatherConditions(): String? {
 private fun WeatherSummary(weather: WeatherResponse?) {
     Box {
         weather?.let {
-            Image(
-                painter = painterResource(it.background()),
-                contentDescription = "Background",
-                contentScale = ContentScale.FillWidth,
-                modifier = Modifier
+            Image(painter = painterResource(it.background()), contentDescription = "Background", contentScale = ContentScale.FillWidth, modifier = Modifier
                     .fillMaxWidth()
-                    .background(Color(0xFF47AB2F))
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+                    .background(Color(0xFF47AB2F)))
+            Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
-                    text = formatTemperature(it.main.temp),
-                    fontSize = 48.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.Serif,
-                    modifier = Modifier.padding(top = 16.dp),
+                        text = formatTemperature(it.main.temp),
+                        fontSize = 48.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold,
+                        fontFamily = FontFamily.Serif,
+                        modifier = Modifier.padding(top = 16.dp),
                 )
-                Text(
-                    text = it.getWeatherConditions().toString(),
-                    fontSize = 24.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.Serif,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-                Text(
-                    text = it.name,
-                    fontSize = 18.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = FontFamily.Serif,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
+                Text(text = it.getWeatherConditions().toString(), fontSize = 24.sp, color = Color.White, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Serif, modifier = Modifier.padding(top = 8.dp))
+                Text(text = it.name, fontSize = 18.sp, color = Color.White, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Serif, modifier = Modifier.padding(top = 8.dp))
                 Spacer(modifier = Modifier.padding(vertical = 8.dp))
             }
 
